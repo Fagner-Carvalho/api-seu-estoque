38aae1853f26a180770a2d305fc78cee80a22bfb
Fagner-Carvalho <fagner.m.carvalho@gmail.com>
2022-03-12 15:35:07 -0300

    initial commit

---
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,36 @@
+import { hash } from "bcrypt";
+import { inject, injectable } from "tsyringe";
+
+import { ICreateUserDTO } from "@modules/accounts/dtos/ICreateUserDTO";
+import { IUsersRepository } from "@modules/accounts/repositories/IUsersRepository";
+import { AppError } from "@shared/errors/AppError";
+
+@injectable()
+class CreateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRespository: IUsersRepository
+  ) {}
+
+  async execute({
+    name,
+    email,
+    password,
+  }: ICreateUserDTO): Promise<void> {
+    const userAlreadyExists = await this.usersRespository.findByEmail(email);
+
+    if (userAlreadyExists) {
+      throw new AppError("User already exists!");
+    }
+
+    const passwordHash = await hash(password, 8);
+
+    await this.usersRespository.create({
+      name,
+      email,
+      password: passwordHash,
+    });
+  }
+}
+
+export { CreateUserUseCase };
