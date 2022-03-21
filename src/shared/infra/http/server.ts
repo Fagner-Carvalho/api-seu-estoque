47b91eece83165b9d167d687bb2b7f49dc75f300
Fagner-Carvalho <fagner.m.carvalho@gmail.com>
2022-03-20 23:13:21 -0300

    add: added colors on server

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata";
 
+import cors from 'cors';
 import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import * as swaggerUi from "swagger-ui-express";
@@ -13,6 +14,7 @@ import * as swaggerFile from "../../../swagger.json";
 
 createConnection();
 const app = express();
+app.use(cors())
 
 app.use(express.json());
 
