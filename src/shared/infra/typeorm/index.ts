af071d4be8d180552d5ea41d7b6102d854662490
Fagner-Carvalho <fagner.m.carvalho@gmail.com>
2022-05-26 14:55:37 -0300

    database docker

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,11 +1,10 @@
 import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
-export default async (host = "database_seu_estoque"): Promise<Connection> => {
+export default async (): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
   return createConnection(
     Object.assign(defaultOptions, {
-      host,
     })
   );
 };
